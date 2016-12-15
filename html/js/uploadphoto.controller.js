b5e0053381731bf17dd5bb494a17e90a8651c6bb
Adam Nolte <ajnolte12@gmail.com>
2016-12-15 04:43:52 +0000

    Add Loading

--- html/js/uploadphoto.controller.js
+++ html/js/uploadphoto.controller.js
@@ -1,13 +1,16 @@
 app.controller('uploadPhotoController', function($scope, $http, $uibModalInstance) {
 
     $scope.croppedImage = null;
+    $scope.loading = false;
     
     $scope.upload = function () {
+        $scope.loading = true;
         $http({
             method: 'POST', 
             url: 'php/addphoto.php',
             data: $scope.croppedImage
         }).success(function (data) {
+            $scope.loading = false;
             $uibModalInstance.close(data);
         });
     }
